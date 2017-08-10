81e19d522a8f44a6cf1e2b8674ccc371a37dc3ee
Coen Hyde <coen.hyde@gmail.com>
2017-08-10 15:19:00 -0700

    fix bug with base64 decoding of VCL

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 set -e
 
 if [[ "${VCL}" ]]; then
-  echo "${VCL}" | base64 --decode > /etc/varnish/default.vcl
+  echo "${VCL}" | base64 -d > /etc/varnish/default.vcl
 fi
 
 exec varnishd -F -f ${VCL_CONFIG} -s malloc,${CACHE_SIZE} ${VARNISHD_PARAMS}
