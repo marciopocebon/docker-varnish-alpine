fc281881d1d67d4e65a90a3d935d9f0a9e232024
Coen Hyde <coen.hyde@gmail.com>
2017-08-11 14:18:09 -0700

    fix bug with varnish logging

--- start.sh
+++ start.sh
@@ -6,5 +6,5 @@ if [[ "${VCL}" ]]; then
   echo "${VCL}" | base64 -d > ${VCL_CONFIG}
 fi
 
-varnishd -F -f ${VCL_CONFIG} -s malloc,${CACHE_SIZE} ${VARNISHD_PARAMS} &&
+(varnishd -F -f ${VCL_CONFIG} -s malloc,${CACHE_SIZE} ${VARNISHD_PARAMS}) &
 varnishncsa
